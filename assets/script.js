77e2ed177c9655ec3186eb401921daf4516402d2
Mauricio Gastón <mauuuricio@hotmail.com>
2024-05-13 21:37:02 -0300

    dede

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,21 @@ console.log('Mi nombre es ' + nombre)
 $("#container")
 
 document.getElementById("container")
+
+function superSum(){
+
+}
+
+
+//! Esrtkjdglfsdfg
+/*
+function superSum(){
+
+}
+*/
+
+//? trdosadoasoda azul
+
+//* resalta
+
+//todo: d{ñlgdfpgs}
